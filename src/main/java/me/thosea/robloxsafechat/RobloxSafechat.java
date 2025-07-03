2dc04b4f5860938ecab732e1797b24d24fac6fc5
thosea <104597976+imthosea@users.noreply.github.com>
2025-07-03 19:47:35 -0400

    nit: change exception message

--- src/main/java/me/thosea/robloxsafechat/RobloxSafechat.java
+++ src/main/java/me/thosea/robloxsafechat/RobloxSafechat.java
@@ -53,7 +53,7 @@ public final class RobloxSafechat implements ClientModInitializer {
 
 		public Builderman goBack() {
 			if(current.parent == null) {
-				throw new IllegalStateException("No parent? Probably went to get the milk.");
+				throw new IllegalStateException("no parent found");
 			}
 
 			current = current.parent;
